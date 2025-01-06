dcbaebadfe82f76e541004c02fcb2712a17e219d
J. Junior <junior.jaava@gmail.com>
2025-01-06 17:10:15 -0300

    private set

--- Calculator/src/main/kotlin/com/rocksolidknowledge/Answer.kt
+++ Calculator/src/main/kotlin/com/rocksolidknowledge/Answer.kt
@@ -3,7 +3,7 @@ package com.rocksolidknowledge
 class Answer(id: Int, user: User) {
 
     var votes: Int = 0
-        private set
+        private set // private attributes, with public access modified
 
     fun vote(direction: VoteEnum) {
         when (direction) {
