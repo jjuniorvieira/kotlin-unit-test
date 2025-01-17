df07a090fb57ad240c4b9e94a48fbb6dd56dad07
J. Junior <junior.jaava@gmail.com>
2025-01-16 21:36:11 -0300

    parameterized tests

--- Calculator/src/test/kotlin/com/rocksolidknowledge/QuestionTest.kt
+++ Calculator/src/test/kotlin/com/rocksolidknowledge/QuestionTest.kt
@@ -1,24 +1,34 @@
 package com.rocksolidknowledge
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
+@Suppress("ClassName")
 class QuestionTest {
 
-    @Test
-    fun `should have a valid title`() {
-        val user = User(1, "Kevin", 0)
+    @Nested
+    inner class questions {
+        @ParameterizedTest()
+        @EmptySource
+        @ValueSource(strings = [" ", "   ", "\n", "\t"])
+        fun `should have a valid title`(title: String) {
+            val user = User(1, "Kevin", 0)
 
 
-        assertThrows(QuestionException::class.java) { Question(1, user, "", "ggg") }
-    }
+            assertThrows(QuestionException::class.java) { Question(1, user, title, "ggg") }
+        }
 
-    @Test
-    fun `should have a valid discussion`() {
-        val user = User(1, "Kevin", 0)
+        @ParameterizedTest
+        @EmptySource
+        fun `should have a valid discussion`(discussion: String) {
+            val user = User(1, "Kevin", 0)
 
 
-        assertThrows(QuestionException::class.java) { Question(1, user, "fff", "") }
+            assertThrows(QuestionException::class.java) { Question(1, user, "fff", discussion) }
+        }
     }
 
 }
--- Calculator/src/test/kotlin/com/rocksolidknowledge/UserTest.kt
+++ Calculator/src/test/kotlin/com/rocksolidknowledge/UserTest.kt
@@ -1,44 +1,55 @@
+@file:Suppress("ClassName")
+
 package com.rocksolidknowledge
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-class UserTest {
+class `A user` {
 
-    @Test
-    fun shouldIncreaseReputationWhenQuestionOrAnswerIsVotedUp() {
-        val user = User(1, "Kevin", 0)
+    @Nested
+    inner class `should increase` {
 
-        user.questionOrAnswerVotedOn(VoteEnum.Up)
+        @Test
+        fun `reputation when question or answer is voted up`() {
+            val user = User(1, "Kevin", 0)
 
-        assertEquals(1, user.reputation)
+            user.questionOrAnswerVotedOn(VoteEnum.Up)
+
+            assertEquals(1, user.reputation)
+        }
+
+        @Test
+        fun `reputation when question or answer is voted up and the user has an initial reputation`() {
+            val user = User(1, "Kevin", 10)
+
+            user.questionOrAnswerVotedOn(VoteEnum.Up)
+
+            assertEquals(11, user.reputation)
+        }
     }
 
-    @Test
-    fun shouldIncreaseReputationWhenQuestionOrAnswerIsVotedUpAndTheUserHasAnInitialReputation() {
-        val user = User(1, "Kevin", 10)
+    @Nested
+    inner class `should decrease` {
 
-        user.questionOrAnswerVotedOn(VoteEnum.Up)
+        @Test
+        fun `reputation when question or answer is voted down`() {
+            val user = User(1, "Kevin", 0)
 
-        assertEquals(11, user.reputation)
-    }
+            user.questionOrAnswerVotedOn(VoteEnum.Down)
 
-    @Test
-    fun shouldDecreaseReputationWhenQuestionOrAnswerIsVotedDown() {
-        val user = User(1, "Kevin", 0)
+            assertEquals(-1, user.reputation)
+        }
 
-        user.questionOrAnswerVotedOn(VoteEnum.Down)
+        @Test
+        fun `reputation when question or answer is voted down and the user has an initial reputation`() {
+            val user = User(1, "Kevin", 10)
 
-        assertEquals(-1, user.reputation)
-    }
+            user.questionOrAnswerVotedOn(VoteEnum.Down)
 
-    @Test
-    fun shouldDecreaseReputationWhenQuestionOrAnswerIsVotedDownAndTheUserHasAnInitialReputation() {
-        val user = User(1, "Kevin", 10)
-
-        user.questionOrAnswerVotedOn(VoteEnum.Down)
-
-        assertEquals(9, user.reputation)
+            assertEquals(9, user.reputation)
+        }
     }
 
 }
