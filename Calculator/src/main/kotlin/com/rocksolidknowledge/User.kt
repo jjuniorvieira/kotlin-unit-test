347522f6fac5f43f56600fee85c9e96b74c4ce05
J. Junior <junior.jaava@gmail.com>
2025-01-31 21:44:20 +0100

    using ArgumentsSource

--- Calculator/src/main/kotlin/com/rocksolidknowledge/User.kt
+++ Calculator/src/main/kotlin/com/rocksolidknowledge/User.kt
@@ -2,6 +2,11 @@ package com.rocksolidknowledge
 
 data class User(val id: Int, val name: String, var reputation: Int) {
 
+    init {
+        if(id <= 0) throw IllegalArgumentException()
+        if(name.trim().isEmpty()) throw IllegalArgumentException()
+    }
+
     fun questionOrAnswerVotedOn(direction: VoteEnum) {
         when (direction) {
             VoteEnum.Up -> reputation++
--- Calculator/src/test/kotlin/com/rocksolidknowledge/UserTest.kt
+++ Calculator/src/test/kotlin/com/rocksolidknowledge/UserTest.kt
@@ -2,11 +2,39 @@
 
 package com.rocksolidknowledge
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.lang.IllegalArgumentException
+import java.util.stream.Stream
+
+class UserArgumentProvider : ArgumentsProvider {
+    override fun provideArguments(extensionContext: ExtensionContext?): Stream<out Arguments> {
+        return Stream.of(
+            Arguments.of(0, "Kevin"),
+            Arguments.of(-1, "Kevin"),
+            Arguments.of(1, ""),
+            Arguments.of(1, " "),
+            Arguments.of(1, "\t"),
+            Arguments.of(1, "\n"),
+        )
+    }
+
+}
 
 class `A user` {
+
+    @ParameterizedTest(name = "Id: {0}, Name: {1}")
+    @ArgumentsSource(UserArgumentProvider::class)
+    fun `should be valid`(id: Int, name: String) {
+        Assertions.assertThrows(IllegalArgumentException::class.java) {User(id, name, 0)}
+    }
 
     @Nested
     inner class `should increase` {
